ca0a367af9f3368d4584245ba7523a9cac8b8a03
kacha930 <robiwere20@gmail.com>
2024-07-01 12:21:56 +0300

    added a html file and used getElementById

--- speed.js
+++ speed.js
@@ -1,17 +1,22 @@
-function speedDetector(speed) {
+function speedDetector() {
+    //we use .value to get the value back rather than .innerHTML to print the value
+    let speed = document.getElementById('speed').value;
     const speedLimit = 70;
     let kmPerDemeritPoint = 5;
     const pointsLimitForSuspension = 12;
-// 70 km/hr is like the constant/fixed mark. if it changes a condition must apply.
-    if (speed < speedLimit) {
-        console.log("OK");
-        return 0; // else create a formula for calculating points deduction according to the speed travelled
+    // 70 km/hr is like the constant/fixed mark. if it changes a condition must apply.
+    if (speed <= speedLimit) {
+        document.getElementById('print').innerHTML = "OK";
+        return 0; 
+        // else create a formula for calculating points deduction according to the speed travelled
     } else {
         const pointsAboveLimit = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
-        console.log(`Points: ${pointsAboveLimit}`);
+        document.getElementById('print').innerHTML = `Points: ${pointsAboveLimit}`;
+       
         
         if (pointsAboveLimit > pointsLimitForSuspension) {
-            console.log("LICENSE SUSPENDED");
+            document.getElementById('print').innerHTML = "LICENCE SUSPENDED";
+            
         }
         
         return pointsAboveLimit;
